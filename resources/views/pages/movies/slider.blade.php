e9540bd363892e319255478496edadb9bad0bf40
Yasir Raheel <yasir@cineworm.online>
2026-01-06 03:48:54 +0500

    Update movies slider layout with Player, News Ticker and Pacman Game

--- resources/views/pages/movies/slider.blade.php
+++ resources/views/pages/movies/slider.blade.php
@@ -1,52 +1,193 @@
-<!-- Start Slider Area -->
-<div class="slider-area pt-3">
-  <div class="container-fluid">
-    <div class="row">
-      <div class="splide">
-    <div class="splide__track">
-      <ul class="splide__list">
-        @foreach($slider as $slider_data)
+@php
+    if(!isset($movies_info)){
+        $movies_info = \App\Movies::where('status', 1)
+            ->where('upcoming', 0)
+            ->inRandomOrder()
+            ->first();
+    }
 
-        @if($slider_data->slider_post_id=="")
+    if(!isset($news_tickers)){
+        $news_tickers = \App\Models\NewsTicker::where('status', 1)->orderBy('id', 'DESC')->get();
+    }
+@endphp
 
-        <?php $slider_url='#';?>
+<link rel="stylesheet" type="text/css" href="{{ URL::asset('site_assets/player/content/global.css') }}">
+<script type="text/javascript" src="{{ URL::asset('site_assets/player/java/' . $FWDEVPlayer) }}"></script>
+<!-- Include FontAwesome for the donate icon -->
+<script src="https://kit.fontawesome.com/a076d05399.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 
-        @elseif($slider_data->slider_type=="Movies")
+<style>
+    .news-ticker-container {
+        background: #1a1a1a;
+        height: 250px;
+        overflow-y: auto;
+        padding: 15px;
+        color: #fff;
+        margin-top: 0;
+    }
+    .news-item {
+        margin-bottom: 20px;
+        border-bottom: 1px solid #333;
+        padding-bottom: 15px;
+    }
+    .news-item:last-child {
+        border-bottom: none;
+    }
+    .news-headline {
+        font-size: 16px;
+        font-weight: bold;
+        margin-bottom: 5px;
+        color: #e50914; /* Netflix red or your brand color */
+    }
+    .news-details {
+        font-size: 14px;
+        color: #ccc;
+        line-height: 1.4;
+    }
+    .news-time {
+        font-size: 12px;
+        color: #888;
+        margin-top: 5px;
+        display: block;
+    }
+    .breaking-badge {
+        background-color: #e50914;
+        color: white;
+        padding: 2px 6px;
+        border-radius: 4px;
+        font-size: 10px;
+        text-transform: uppercase;
+        margin-right: 5px;
+        vertical-align: middle;
+    }
+    /* Scrollbar styling */
+    .news-ticker-container::-webkit-scrollbar {
+        width: 6px;
+    }
+    .news-ticker-container::-webkit-scrollbar-track {
+        background: #111;
+    }
+    .news-ticker-container::-webkit-scrollbar-thumb {
+        background: #444;
+        border-radius: 3px;
+    }
+    .news-ticker-container::-webkit-scrollbar-thumb:hover {
+        background: #666;
+    }
 
-        <?php $slider_url= URL::to('movies/details/'.App\Movies::getMoviesInfo($slider_data->slider_post_id,'video_slug').'/'.$slider_data->slider_post_id);?>
+    /* Pacman Game Container Styling */
+    .pacman-game-container {
+        background: #1a1a1a;
+        height: 100%;
+        overflow: hidden;
+        padding: 0;
+        color: #fff;
+    }
 
-        @elseif($slider_data->slider_type=="Shows")
+    .pacman-game-wrapper {
+        background: #000;
+        height: 300px; /* Adjusted height to fit with ticker under player */
+        border-radius: 5px;
+        overflow: hidden;
+        position: relative;
+    }
 
-        <?php $slider_url= URL::to('shows/details/'.App\Series::getSeriesInfo($slider_data->slider_post_id,'series_slug').'/'.$slider_data->slider_post_id);?>
+    .pacman-game-wrapper iframe {
+        width: 100%;
+        height: 100%;
+        border: none;
+        display: block;
+    }
+</style>
 
-        @elseif($slider_data->slider_type=="Sports")
-
-        <?php $slider_url= URL::to('sports/details/'.App\Sports::getSportsInfo($slider_data->slider_post_id,'video_slug').'/'.$slider_data->slider_post_id);?>
-
-        @elseif($slider_data->slider_type=="LiveTV")
-
-        <?php $slider_url= URL::to('livetv/details/'.App\LiveTV::getLiveTvInfo($slider_data->slider_post_id,'channel_slug').'/'.$slider_data->slider_post_id);?>
-
-        @else
-          <?php $slider_url='#';?>
-        @endif
-
-        <li class="splide__slide">
-          <a href="{{$slider_url}}" title="{{stripslashes($slider_data->slider_title)}}">
-            <div class="splide-slider-details-area">
-              <h1>{{stripslashes($slider_data->slider_title)}}</h1>
-              <a href="{{$slider_url}}" class="btn-watch" title="{{stripslashes($slider_data->slider_title)}}"><img src="{{ URL::asset('site_assets/images/ic-play.png') }}" alt="ic-play" title="ic-play">{{trans('words.watch')}}</a>
-              {{-- <a href="{{ URL::to('membership_plan') }}" class="btn-buy-plan" title="buy-plan"><img src="{{ URL::asset('site_assets/images/ic-subscribe.png') }}" alt="ic-subscribe" title="ic-subscribe">{{trans('words.buy_plan')}}</a> --}}
+<div class="slider-area p-0">
+    <div class="container-fluid px-0">
+        <div class="row no-gutters">
+            <!-- Video Player Column -->
+            <div class="col-lg-9 col-md-12" style="padding: 0;">
+                @if ($movies_info && $movies_info->video_url != '')
+                    @if ($movies_info->video_type == 'GoogleDrive')
+                        @include('pages.movies.player.google_drive_player')
+                    @else
+                        @include('pages.movies.player.other')
+                    @endif
+                @else
+                    <div
+                        style="text-align: center; padding: 70px 30px; font-size: 24px; font-weight: 700; background: #101011;
+                               border-radius: 10px; margin-top: 15px; min-height: 280px; line-height: 6;">
+                        NO Source URL Set
+                    </div>
+                @endif
             </div>
-            <img src="{{URL::to('/'.$slider_data->slider_image)}}" title="{{stripslashes($slider_data->slider_title)}}" alt="{{stripslashes($slider_data->slider_title)}}">
-          </a>
-        </li>
-        @endforeach
 
-      </ul>
+            <!-- Right Sidebar -->
+            <div class="col-lg-3 col-md-12 bg-dark" style="padding: 0; border-left: 1px solid #333;">
+
+                <!-- News Ticker Section -->
+                <div class="card bg-dark text-white border-0 mb-2">
+                    <div class="card-header p-2" id="headingNews" style="background: #111; border-bottom: 1px solid #333;">
+                        <h5 class="mb-0">
+                            <button class="btn btn-link text-white text-decoration-none w-100 text-left font-weight-bold" data-toggle="collapse" data-target="#collapseNews" aria-expanded="true" aria-controls="collapseNews">
+                                <i class="fa fa-newspaper-o mr-2"></i> Latest News
+                            </button>
+                        </h5>
+                    </div>
+
+                    <div id="collapseNews" class="collapse show" aria-labelledby="headingNews">
+                        <div class="card-body p-0">
+                            <div class="news-ticker-container" style="height: 300px; border: none; background: transparent;">
+                                @if(isset($news_tickers) && count($news_tickers) > 0)
+                                    @foreach($news_tickers as $news)
+                                        <div class="news-item">
+                                            <div class="news-headline">
+                                                @if($news->is_breaking)
+                                                    <span class="breaking-badge">BREAKING</span>
+                                                @endif
+                                                {{ $news->headline }}
+                                            </div>
+                                            <div class="news-details">
+                                                {!! \Illuminate\Support\Str::limit(strip_tags($news->details), 150) !!}
+                                            </div>
+                                            <span class="news-time">
+                                                <i class="fa fa-clock-o"></i> {{ \Carbon\Carbon::parse($news->created_at)->diffForHumans() }}
+                                            </span>
+                                        </div>
+                                    @endforeach
+                                @else
+                                    <p style="color: #888; padding: 15px;">No news updates at the moment.</p>
+                                @endif
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- Game Section -->
+                <div class="card bg-dark text-white border-0">
+                    <div class="card-header p-2" id="headingGame" style="background: #111; border-bottom: 1px solid #333; border-top: 1px solid #333;">
+                        <h5 class="mb-0">
+                            <button class="btn btn-link collapsed text-white text-decoration-none w-100 text-left font-weight-bold" data-toggle="collapse" data-target="#collapseGame" aria-expanded="true" aria-controls="collapseGame">
+                                <i class="fa fa-gamepad mr-2"></i> Games
+                            </button>
+                        </h5>
+                    </div>
+                    <div id="collapseGame" class="collapse show" aria-labelledby="headingGame">
+                        <div class="card-body p-0">
+                            <div class="pacman-game-container">
+                                <div class="pacman-game-wrapper">
+                                    <iframe
+                                        src="https://pacman.platzh1rsch.ch/"
+                                        style="width: 100%; height: 100%; border: none;"
+                                        allowfullscreen
+                                        title="Pacman Game">
+                                    </iframe>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
     </div>
-    </div>
-    </div>
-  </div>
 </div>
-<!-- End Slider Area -->
