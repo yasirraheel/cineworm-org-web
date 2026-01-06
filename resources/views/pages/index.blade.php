68c3d5df0db8ca0b2f5ae3b85d6050c7d0f95329
Yasir Raheel <yasir@cineworm.online>
2026-01-06 19:24:03 +0500

    Remove games section from homepage and increase news ticker font size

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -246,31 +246,6 @@
                         </div>
                     </div>
                 </div>
-
-                <!-- Game Section -->
-                <div class="card bg-dark text-white border-0 mb-2" style="flex-shrink: 0;">
-                    <div class="card-header p-2" id="headingGame" style="background: #111; border-bottom: 1px solid #333; border-top: 1px solid #333;">
-                        <h5 class="mb-0">
-                            <button class="btn btn-link text-white text-decoration-none w-100 text-left font-weight-bold" type="button" data-bs-toggle="collapse" data-bs-target="#collapseGame" aria-expanded="false" aria-controls="collapseGame">
-                                <i class="fa fa-gamepad mr-2"></i> Games
-                            </button>
-                        </h5>
-                    </div>
-                    <div id="collapseGame" class="collapse" aria-labelledby="headingGame" data-bs-parent="#watchAccordion" style="overflow: hidden; transition: height 0.3s ease;">
-                        <div class="card-body p-0">
-                            <div class="pacman-game-container">
-                                <div class="pacman-game-wrapper" style="height: 200px;">
-                                    <iframe
-                                        src="https://pacman.platzh1rsch.ch/"
-                                        style="width: 100%; height: 100%; border: none;"
-                                        allowfullscreen
-                                        title="Pacman Game">
-                                    </iframe>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
             </div>
 
                 <div class="sidebar-banners-container mt-3">
