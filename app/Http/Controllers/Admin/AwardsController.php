aeb40602f5be7b1c81728633d459e2fc9f89cbd5
Yasir Raheel <yasir@cineworm.online>
2026-01-28 23:37:02 +0500

    Fix undefined page_title variable in AwardsController

--- app/Http/Controllers/Admin/AwardsController.php
+++ app/Http/Controllers/Admin/AwardsController.php
@@ -5,13 +5,25 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Award;
 use Illuminate\Http\Request;
+use Auth;
 
-class AwardsController extends Controller
+class AwardsController extends MainAdminController
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     public function index()
     {
+        if(Auth::User()->usertype!="Admin"){
+            \Session::flash('flash_message', trans('words.access_denied'));
+            return redirect('admin/dashboard');
+        }
+
+        $page_title = 'Awards List';
         $awards = Award::with(['user', 'movie'])->orderBy('id', 'desc')->paginate(15);
-        
-        return view('admin.pages.awards.list', compact('awards'));
+
+        return view('admin.pages.awards.list', compact('awards', 'page_title'));
     }
 }
