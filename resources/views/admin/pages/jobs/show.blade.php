24045f5262e605a25d56126d54ad66b271a5f2ee
Yasir Raheel <yasir@cineworm.online>
2026-06-09 21:05:28 +0500

    Fix dark text on admin job details view

--- resources/views/admin/pages/jobs/show.blade.php
+++ resources/views/admin/pages/jobs/show.blade.php
@@ -21,33 +21,33 @@
                         <div class="card-body">
                             <h4 class="header-title mb-4">{{ $job->title }}</h4>
 
-                            <div class="row mb-3">
-                                <div class="col-md-3"><strong>User:</strong></div>
+                            <div class="row mb-3" style="color: #fff;">
+                                <div class="col-md-3" style="color: #ccc;"><strong>User:</strong></div>
                                 <div class="col-md-9">{{ optional($job->user)->name }} ({{ optional($job->user)->email }})</div>
                             </div>
                             
-                            <div class="row mb-3">
-                                <div class="col-md-3"><strong>Company:</strong></div>
+                            <div class="row mb-3" style="color: #fff;">
+                                <div class="col-md-3" style="color: #ccc;"><strong>Company:</strong></div>
                                 <div class="col-md-9">{{ $job->company }}</div>
                             </div>
 
-                            <div class="row mb-3">
-                                <div class="col-md-3"><strong>Location:</strong></div>
+                            <div class="row mb-3" style="color: #fff;">
+                                <div class="col-md-3" style="color: #ccc;"><strong>Location:</strong></div>
                                 <div class="col-md-9">{{ $job->location }}</div>
                             </div>
 
-                            <div class="row mb-3">
-                                <div class="col-md-3"><strong>Salary:</strong></div>
+                            <div class="row mb-3" style="color: #fff;">
+                                <div class="col-md-3" style="color: #ccc;"><strong>Salary:</strong></div>
                                 <div class="col-md-9">{{ $job->salary ?? 'N/A' }}</div>
                             </div>
 
-                            <div class="row mb-3">
-                                <div class="col-md-3"><strong>Contact Details:</strong></div>
+                            <div class="row mb-3" style="color: #fff;">
+                                <div class="col-md-3" style="color: #ccc;"><strong>Contact Details:</strong></div>
                                 <div class="col-md-9">{{ $job->contact_details ?? 'N/A' }}</div>
                             </div>
 
-                            <div class="row mb-3">
-                                <div class="col-md-3"><strong>Status:</strong></div>
+                            <div class="row mb-3" style="color: #fff;">
+                                <div class="col-md-3" style="color: #ccc;"><strong>Status:</strong></div>
                                 <div class="col-md-9">
                                     @if($job->status == 1)
                                         <span class="badge badge-success">Approved</span>
@@ -57,8 +57,8 @@
                                 </div>
                             </div>
 
-                            <div class="row mb-3">
-                                <div class="col-md-3"><strong>Description:</strong></div>
+                            <div class="row mb-3" style="color: #fff;">
+                                <div class="col-md-3" style="color: #ccc;"><strong>Description:</strong></div>
                                 <div class="col-md-9">{!! nl2br(e($job->description)) !!}</div>
                             </div>
                             
